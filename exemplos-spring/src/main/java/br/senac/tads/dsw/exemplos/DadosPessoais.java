8531f60bf9c7c18f750638480fd26ec00b418e48
ftsuda-senac <ftsuda.senac@gmail.com>
2021-10-19 20:41:54 -0300

    Exemplos Get

---
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplos/DadosPessoais.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.dsw.exemplos;
+
+import java.time.LocalDate;
+
+public class DadosPessoais {
+    
+    private String nome;
+    
+    private String apelido;
+    
+    private LocalDate dataNascimento;
+    
+    private int numero;
+    
+    private String email;
+    
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
--- exemplos-spring/src/main/java/br/senac/tads/dsw/exemplos/ExemploController.java
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplos/ExemploController.java
@@ -5,11 +5,16 @@
  */
 package br.senac.tads.dsw.exemplos;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -59,6 +64,87 @@ public class ExemploController {
         mv.addObject("apelido", apelido);
         mv.addObject("numero", numero);
         mv.addObject("data", data);
+        return mv;
+    }
+    
+    @GetMapping("/ex05")
+    public ModelAndView exemplo05() {
+        
+        DadosPessoais dados = new DadosPessoais();
+        dados.setNome("Fulano da Silva");
+        dados.setApelido("fulano");
+        dados.setDataNascimento(LocalDate.of(2000, 5, 20));
+        dados.setNumero(54);
+        dados.setEmail("fulano@teste.com.br");
+        dados.setTelefone("(11) 98765-1122");
+        
+        ModelAndView mv = new ModelAndView("exemplo05");
+        mv.addObject("dados", dados);
+        return mv;
+    }
+
+    @GetMapping("/ex06")
+    public ModelAndView exemplo06(
+            @RequestParam(value = "apresentacao", defaultValue = "tabela") String modoApresentacao) {
+        
+        DadosPessoais d01 = new DadosPessoais();
+        d01.setNome("Fulano da Silva");
+        d01.setApelido("fulano");
+        d01.setDataNascimento(LocalDate.of(2000, 5, 20));
+        d01.setNumero(54);
+        d01.setEmail("fulano@teste.com.br");
+        d01.setTelefone("(11) 98765-1122");
+        
+        DadosPessoais d02 = new DadosPessoais();
+        d02.setNome("Ciclana de Souza");
+        d02.setApelido("ciclana");
+        d02.setDataNascimento(LocalDate.of(1998, 3, 15));
+        d02.setNumero(87);
+        d02.setEmail("ciclana@teste.com.br");
+        d02.setTelefone("(11) 91234-9988");
+        
+        DadosPessoais d03 = new DadosPessoais();
+        d03.setNome("Beltrano das Cruzes");
+        d03.setApelido("beltrano");
+        d03.setDataNascimento(LocalDate.of(2001, 12, 5));
+        d03.setNumero(36);
+        d03.setEmail("beltrano@teste.com.br");
+        d03.setTelefone("(11) 97654-1234");
+        
+        List<DadosPessoais> lista = new ArrayList<>();
+        lista.add(d01);
+        lista.add(d02);
+        lista.add(d03);
+        
+        String arquivoView;
+        if ("lista".equalsIgnoreCase(modoApresentacao)){
+            // Mostrar as informações em uma lista não ordenada (<ul>)
+            arquivoView = "exemplo06-lista";
+        } else {
+            // Mostrar as informaçoes em uma <table>
+            arquivoView = "exemplo06-tabela";
+        }
+        
+        ModelAndView mv = new ModelAndView(arquivoView);
+        mv.addObject("itens", lista);
+        return mv;
+    }
+    
+    @GetMapping("/ex07")
+    public ModelAndView exemplo07(@RequestHeader("user-agent") String userAgent) {
+        
+        boolean clientMobile = userAgent.toLowerCase().contains("mobile");
+        
+        ModelAndView mv = new ModelAndView("exemplo07");
+        mv.addObject("mobile", clientMobile);
+        mv.addObject("ua", userAgent);
+        return mv;
+    }
+    
+    @GetMapping("/ex08")
+    public ModelAndView exemplo08(@RequestHeader Map<String, String> cabecalhos) {
+        ModelAndView mv = new ModelAndView("exemplo08");
+        mv.addObject("cabecalhos", cabecalhos);
         return mv;
     }
 
