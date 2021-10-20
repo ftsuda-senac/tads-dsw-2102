dfe33f3fe4504b263e2a63104ea49a587e9a8e98
ftsuda-senac <ftsuda.senac@gmail.com>
2021-10-19 22:31:24 -0300

    Exemplo de carregamento do formulario preenchido

--- exemplos-spring/src/main/java/br/senac/tads/dsw/exemplos/ExemploPostController.java
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplos/ExemploPostController.java
@@ -5,9 +5,14 @@
  */
 package br.senac.tads.dsw.exemplos;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,6 +29,32 @@ public class ExemploPostController {
     @GetMapping
     public ModelAndView mostrarForm() {
         ModelAndView mv = new ModelAndView("formulario-bs");
+        mv.addObject("dados", new DadosPessoais());
+        return mv;
+    }
+    
+    @GetMapping("/{id}")
+    public ModelAndView mostrarFormPreenchido(@PathVariable Integer id) {
+        DadosPessoais dados = new DadosPessoais();
+        dados.setId(id);
+        dados.setNome("Ciclana de Souza");
+        dados.setDescricao("Descrição da Ciclana");
+        dados.setDataNascimento(LocalDate.of(1998, 3, 20));
+        dados.setEmail("ciclana@teste.com.br");
+        dados.setTelefone("(11) 99999-9988");
+        dados.setSenha("abcd1234");
+        dados.setRepetirSenha("abcd1234");
+        dados.setNumero(15);
+        dados.setAltura(new BigDecimal("1.68"));
+        dados.setPeso(new BigDecimal("76.2"));
+        dados.setGenero(0);
+        List<String> interesses = new ArrayList<>();
+        interesses.add("Gastronomia");
+        interesses.add("Viagens");
+        dados.setInteresses(interesses);
+        
+        ModelAndView mv = new ModelAndView("formulario-bs");
+        mv.addObject("dados", dados);
         return mv;
     }
     
