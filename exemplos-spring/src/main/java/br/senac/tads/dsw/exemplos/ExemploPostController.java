a53cd02c9fb7e819989026a34d6195fce0ac0463
ftsuda-senac <ftsuda.senac@gmail.com>
2021-10-19 22:15:06 -0300

    Exemplos POST com POST-REDIRECT-GET

--- exemplos-spring/src/main/java/br/senac/tads/dsw/exemplos/ExemploPostController.java
+++ exemplos-spring/src/main/java/br/senac/tads/dsw/exemplos/ExemploPostController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -31,6 +32,21 @@ public class ExemploPostController {
         ModelAndView mv = new ModelAndView("resultado-bs");
         mv.addObject("dados", dados);
         return mv;
+    }
+    
+    @PostMapping("/salvar-post-redirect-get")
+    public ModelAndView salvarDadosComPostRedirectGet(@ModelAttribute DadosPessoais dados, 
+            RedirectAttributes redirectAttributes) {
+        // Escopo Flash - Mantém as informações entre duas requisições consecutivas
+        redirectAttributes.addFlashAttribute("dados", dados);
+        
+        ModelAndView mv = new ModelAndView("redirect:/exemplo-post/resultado");
+        return mv;
+    }
+    
+    @GetMapping("/resultado")
+    public ModelAndView mostrarResultado() {
+        return new ModelAndView("resultado-bs");
     }
     
 }
